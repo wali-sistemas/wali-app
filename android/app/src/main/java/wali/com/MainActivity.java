1b9abd2096d39a1c8ad1f04c298a0c7461221e21
wali <46688722+wali-sistemas@users.noreply.github.com>
2023-02-03 11:31:55 -0500

    Subir cambios

---
+++ android/app/src/main/java/wali/com/MainActivity.java
@@ -0,0 +1,5 @@
+package wali.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'wali.com',
+  appName: 'wali',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
